af70090ed060ef6c4488db8b5345ee6b021967a9
Nguyen Huu Kim <kimnh@webee.asia>
2022-10-27 10:05:40 +0700

    feat: mock API with Mock Service Worker

--- src/components/InboxScreen.stories.js
+++ src/components/InboxScreen.stories.js
@@ -4,6 +4,9 @@ import { Provider } from 'react-redux';
 import InboxScreen from './InboxScreen';
 import store from '../lib/store';
 
+import { rest } from 'msw';
+import { MockedState } from './TaskList.stories';
+
 export default {
   title: 'InboxScreen',
   component: InboxScreen,
@@ -13,4 +16,29 @@ export default {
 const Template = (args) => <InboxScreen {...args} />;
 
 export const Default = Template.bind({});
+Default.parameters = {
+  msw: {
+    handlers: [
+      rest.get(
+        'https://jsonplaceholder.typicode.com/todos?userId=1',
+        (req, res, ctx) => {
+          return res(ctx.json(MockedState.tasks));
+        }
+      ),
+    ],
+  },
+};
+
 export const Error = Template.bind({});
+Error.parameters = {
+  msw: {
+    handlers: [
+      rest.get(
+        'https://jsonplaceholder.typicode.com/todos?userId=1',
+        (req, res, ctx) => {
+          return res(ctx.status(403));
+        }
+      ),
+    ],
+  },
+};
