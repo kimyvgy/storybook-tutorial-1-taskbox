2c55ee5fa8b78a19e9b1e37e6a4a236ad75fdd86
Nguyen Huu Kim <kimnh@webee.asia>
2022-10-27 09:32:20 +0700

    feat: inbox screen

---
+++ src/components/InboxScreen.stories.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+
+import InboxScreen from './InboxScreen';
+import store from '../lib/store';
+
+export default {
+  title: 'InboxScreen',
+  component: InboxScreen,
+  decorators: [(story) => <Provider store={store}>{story()}</Provider>],
+}
+
+const Template = (args) => <InboxScreen {...args} />;
+
+export const Default = Template.bind({});
+export const Error = Template.bind({});
