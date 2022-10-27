c39866171c2441cbc63b88079902594172d4aaba
Nguyen Huu Kim <kimnh@webee.asia>
2022-10-27 10:15:19 +0700

    feat: add interactions test

--- src/components/InboxScreen.stories.js
+++ src/components/InboxScreen.stories.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 
+import {
+ fireEvent,
+ within,
+ waitFor,
+ waitForElementToBeRemoved
+} from '@storybook/testing-library';
+
 import InboxScreen from './InboxScreen';
 import store from '../lib/store';
 
@@ -27,6 +34,18 @@ Default.parameters = {
       ),
     ],
   },
+};
+Default.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement);
+  // Waits for the component to transition from the loading state
+  await waitForElementToBeRemoved(await canvas.findByTestId('loading'));
+  // Waits for the component to be updated based on the store
+  await waitFor(async () => {
+    // Simulates pinning the first task
+    await fireEvent.click(canvas.getByLabelText('pinTask-1'));
+    // Simulates pinning the third task
+    await fireEvent.click(canvas.getByLabelText('pinTask-3'));
+  });
 };
 
 export const Error = Template.bind({});
